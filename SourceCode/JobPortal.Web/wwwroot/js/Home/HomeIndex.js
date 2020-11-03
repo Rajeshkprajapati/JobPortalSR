f46420d55e30c9a18a427f844700b1fa92162895
shivanandvits@rediffmail.com <shivanandvits@gmil.com>
2020-11-03 21:07:25 +0530

    sk: muliselect changes and social url

--- SourceCode/JobPortal.Web/wwwroot/js/Home/HomeIndex.js
+++ SourceCode/JobPortal.Web/wwwroot/js/Home/HomeIndex.js
@@ -59,26 +59,6 @@
     );
     $("ul.multiselect-container li").not('li:first').remove();
 
-    //jQuery.fn.multiselect = function () {
-    //    $(this).each(function () {
-    //        var checkboxes = $(this).find("input:checkbox");
-    //        //checkboxes.each(function () {
-    //        //    var checkbox = $(this);
-    //        //    // Highlight pre-selected checkboxes
-    //        //    if (checkbox.prop("checked"))
-    //        //        checkbox.parent().addClass("multiselect-on");
-
-    //        //    // Highlight checkboxes that the user selects
-    //        //    checkbox.click(function () {
-    //        //        if (checkbox.prop("checked"))
-    //        //            checkbox.parent().addClass("multiselect-on");
-    //        //        else
-    //        //            checkbox.parent().removeClass("multiselect-on");
-    //        //    });
-    //        //});
-    //        var data = $('.multiselect-container li.active').length();
-    //    });
-    //};
 
     $('.slidepartners').slick({
         slidesToShow: 1,
